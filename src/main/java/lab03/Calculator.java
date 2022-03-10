d656d3f2696b910303455acbd6f15cf895bd59ff
Anna Kachmasheva <kachmann@fel.cvut.cz>
2022-03-10 11:35:41 +0100

    Added DU3

---
+++ src/main/java/lab03/Calculator.java
@@ -0,0 +1,19 @@
+package lab03;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public int divide(int a, int b) throws Exception {
+        if (b != 0)
+            return a / b;
+        throw new Exception("Divide by 0!");
+    }
+}
