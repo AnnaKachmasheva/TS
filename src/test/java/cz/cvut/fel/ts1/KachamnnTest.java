a91c519b0b2442b142c9a50ac8699ac94f641042
Anna Kachmasheva <kachmann@fel.cvut.cz>
2022-03-10 11:33:01 +0100

    Added DU1

---
+++ src/test/java/cz/cvut/fel/ts1/KachamnnTest.java
@@ -0,0 +1,15 @@
+package cz.cvut.fel.ts1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KachamnnTest {
+
+    @Test
+    void factorialTest() {
+        Kachamnn kachamnn = new Kachamnn();
+        long result = kachamnn.factorial(3);
+        assertEquals(result, 6);
+    }
+}
